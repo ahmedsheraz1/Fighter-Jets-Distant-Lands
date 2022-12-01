667c041c7a2f5e483ceb4df2f4fcea7af271182e
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-01 06:19:14 +0500

    Create Fighter Jets Distant Lands.py

---
+++ Fighter Jets Distant Lands.py
@@ -0,0 +1,23 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hello World!")
+Hello World!
+print("Deep into distant woodlands winds a mazy way, reaching to overlapping spurs of mountains bathed in their hill-side blue.")
+Deep into distant woodlands winds a mazy way, reaching to overlapping spurs of mountains bathed in their hill-side blue.
+mirthful_addition = 12381 + 91817
+amazing_subtraction = 981 - 312
+trippy_multiplication = 38 * 902
+happy_division = 540 / 45
+sassy_combinations = 129 * 1345 + 120 / 6 - 12
+quotient = 6/2
+# the value of quotient is now 3, which makes sense
+address_string = """136 Whowho Rd
+Apt 7
+Whosville, WZ 44494"""
+
+def shout(phrase):
+    if phrase == phrase.upper():
+        return "YOU'RE SHOUTING!"
+    else:
+        return "Can you speak up?"
+    shout("I'M INTERESTED IN SHOUTING")
